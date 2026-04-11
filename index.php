046e966c253ad5629750303f95d34ed935d55016
AminBadani <ahmadaminbadani@gmail.com>
2026-04-11 21:05:32 +0700

    update halaman home, dan membuat helper serta event listener javascript

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
-<?php include 'connection.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Pemesanan Tiket Kerete Api - Home</title>
+    <title>Sistem Pemesanan Tiket Kereta - Home</title>
     <link rel="stylesheet" href="public/style.css">
     <script src="public/script.js" defer></script>
 </head>
 <body>
-    <h1 class="title">Sistem Pemesanan Tiket Kereta Api - Home</h1>
+    <h1 class="title">Sistem Pemesanan Tiket Kereta - Home</h1>
     
+    <nav>
+        <button id="button-jadwal">[ Jadwal ]</button>
+        <button id="button-kereta">[ Kereta ]</button>
+        <button id="button-stasiun">[ Stasiun ]</button>
+    </nav>
+
     <div class="container">
-        <div id="list-ticket"></div>
+        <div id="list-card"></div>
 
         <div class="sidebar">
             <div class="sticky-sidebar">
--- public/script.js
+++ public/script.js
@@ -1,5 +1,25 @@
-// -- Element
-const listTicket = document.getElementById("list-ticket");
+// -- Typedef --
+/**
+ * @typedef {{
+ *      id_jadwal: number, waktu_berangkat: string, waktu_tiba: string, harga_dasar: number,
+ *      id_kereta: string, nama_kereta: string, tipe_kereta: string, kapasitas_total: number,
+ *      id_stasiun_asal: string, nama_stasiun_asal: string, kota_asal: string,
+ *      id_stasiun_tujuan: string, nama_stasiun_tujuan: string, kota_tujuan: string
+ * }} Jadwal
+ *
+ * @typedef {{ id_kereta: string, nama_kereta: string, tipe_kereta: string, kapasitas_total: number }} Kereta
+ * 
+ * @typedef {{ id_stasiun: string, nama_stasiun: string, kota: string }} Stasiun
+ * 
+ */
+
+// -- Element --
+const listCard = document.getElementById("list-card");
+
+const buttonJadwal = document.getElementById("button-jadwal");
+const buttonKereta = document.getElementById("button-kereta");
+const buttonTiket = document.getElementById("button-tiket");
+const buttonStasiun = document.getElementById("button-stasiun");
 
 // -- Helper --
 /**
@@ -7,30 +27,64 @@ const listTicket = document.getElementById("list-ticket");
  *
  * @async
  * @function
- * @returns {{
- *      id_jadwal: number, waktu_berangkat: string, waktu_tiba: string, harga_dasar: number,
- *      id_kereta: string, nama_kereta: string, tipe_kereta: string, kapasitas_total: number,
- *      id_stasiun_asal: string, nama_stasiun_asal: string, kota_asal: string,
- *      id_stasiun_tujuan: string, nama_stasiun_tujuan: string, kota_tujuan: string
- * }[]}
+ * @returns {Jadwal[]}
  */
 async function getAllJadwal() {
     try {
-        const response = await fetch("process/select.php");
+        const response = await fetch("process/select-jadwal.php");
         const data = await response.json();
 
-        console.log(data);
         return data;
     } catch (error) {
         console.error(error);
     }
 }
 
-// -- Main | Call --
-window.onload = async () => {
-    const allJadwal = await getAllJadwal();
+/**
+ * Get all data from table kereta
+ *
+ * @async
+ * @function
+ * @returns {Kereta[]}
+ */
+async function getAllKereta() {
+    try {
+        const response = await fetch("process/select-kereta.php");
+        const data = await response.json();
 
-    for (const jadwal of allJadwal) {
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Get all data from table stasiun
+ *
+ * @async
+ * @function
+ * @returns {Stasiun[]}
+ */
+async function getAllStasiun() {
+    try {
+        const response = await fetch("process/select-stasiun.php");
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Render list for data jadwal
+ *
+ * @param {Jadwal[]} list
+ * @param {HTMLElement} renderElement
+ */
+function renderListJadwal(list, renderElement) {
+    renderElement.innerHTML = ''
+    for (const jadwal of list) {
         const hargaRupiah = Number(jadwal.harga_dasar).toLocaleString("id-ID");
         const dateOptions = {
             weekday: "long",
@@ -40,31 +94,104 @@ window.onload = async () => {
             hour: "2-digit",
             minute: "2-digit",
             second: "2-digit",
-            hour12: false
+            hour12: false,
         };
-        const tanggalBerangkat = new Date(jadwal.waktu_berangkat).toLocaleString("id-ID", dateOptions);
-        const tanggalTiba = new Date(jadwal.waktu_tiba).toLocaleString("id-ID", dateOptions);
+        const waktuBerangkat = new Date(jadwal.waktu_berangkat).toLocaleString("id-ID", dateOptions);
+        const waktuTiba = new Date(jadwal.waktu_tiba).toLocaleString("id-ID", dateOptions);
 
-        const cardTicket = `
-            <div class="card-ticket" key="${jadwal.id_kereta}">
+        const cardJadwal = `
+            <div class="item-card" key="${jadwal.id_jadwal}">
                 <div class="card-header">
-                    <h3>${jadwal.nama_kereta}</h3>
+                    <h3>
+                        <a href="detail.php?id-jadwal=${jadwal.id_jadwal}">${jadwal.nama_kereta}</a>
+                    </h3>
+                    <p>Tipe kereta: ${jadwal.tipe_kereta}</p>
+                    <p>Harga tiket: Rp${hargaRupiah}</p>
                     <p>Stasiun asal: ${jadwal.nama_stasiun_asal} - (${jadwal.kota_asal})</p>
                     <p>Stasiun tujuan: ${jadwal.nama_stasiun_tujuan} - (${jadwal.kota_tujuan})</p>
                 </div>
                 <div class="card-content">
-                    <p>Tipe kereta: ${jadwal.tipe_kereta}</p>
-                    <p>Harga tiket: Rp${hargaRupiah}</p>
-                    <p>Berangkat: ${tanggalBerangkat}</p>
-                    <p>Tiba: ${tanggalTiba}</p>
-                </div>
-                <div class="card-footer">
+                    <p>Berangkat: ${waktuBerangkat}</p>
+                    <p>Tiba: ${waktuTiba}</p>
                     <p>Kapasitas: ${jadwal.kapasitas_total}</p>
                     <p>Terisi: 20</p>
                     <p>Sisa: 30</p>
                 </div>
+                <div class="card-footer">
+                    <button>Edit kereta</button>
+                    <button>Hapus kereta</button>
+                </div>
             </div>
         `;
-        listTicket.innerHTML += cardTicket;
+        renderElement.innerHTML += cardJadwal;
     }
-};
+}
+
+/**
+ * Render list for data kereta
+ *
+ * @param {Kereta[]} list
+ * @param {HTMLElement} renderElement
+ */
+function renderListKereta(list, renderElement) {
+    renderElement.innerHTML = ''
+    for (const kereta of list) {
+        const cardKereta = `
+            <div class="item-card" key="${kereta.id_kereta}">
+                <div class="card-header">
+                    <h3>${kereta.nama_kereta}</h3>
+                </div>
+                <div class="card-content">
+                    <p>Tipe kereta: ${kereta.tipe_kereta}</p>
+                    <p>Kapasitas: ${kereta.kapasitas_total}</p>
+                </div>
+                <div class="card-footer">
+                    <button>Edit kereta</button>
+                    <button>Hapus kereta</button>
+                </div>
+            </div>
+        `;
+        renderElement.innerHTML += cardKereta;
+    }
+}
+
+/**
+ * Render list for data stasiun
+ *
+ * @param {Stasiun[]} list
+ * @param {HTMLElement} renderElement
+ */
+function renderListStasiun(list, renderElement) {
+    renderElement.innerHTML = ''
+    for (const stasiun of list) {
+        const cardStasiun = `
+            <div class="item-card" key="${stasiun.id_stasiun}">
+                <div class="card-header">
+                    <h3>${stasiun.nama_stasiun}</h3>
+                </div>
+                <div class="card-content">
+                    <p>Kota: ${stasiun.kota}</p>
+                </div>
+                <div class="card-footer">
+                    <button>Edit stasiun</button>
+                    <button>Hapus stasiun</button>
+                </div>
+            </div>
+        `;
+        renderElement.innerHTML += cardStasiun;
+    }
+}
+
+
+// -- Main | Call --
+window.onload = async () => {
+    const allJadwal = await getAllJadwal();
+    const allKereta = await getAllKereta();
+    const allStasiun = await getAllStasiun();
+
+    renderListJadwal(allJadwal, listCard)
+    
+    buttonJadwal.addEventListener('click', () => renderListJadwal(allJadwal, listCard)) 
+    buttonKereta.addEventListener('click', () => renderListKereta(allKereta, listCard)) 
+    buttonStasiun.addEventListener('click', () => renderListStasiun(allStasiun, listCard)) 
+}
